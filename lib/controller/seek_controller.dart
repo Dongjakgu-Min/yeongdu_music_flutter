f02af68432b353a52b3c72801e842f313d1d7164
Song <songyw9812@gmail.com>
2022-06-14 21:44:57 +0900

    change streambuilder to obx

--- lib/controller/seek_controller.dart
+++ lib/controller/seek_controller.dart
@@ -1,22 +1,29 @@
 import 'package:flutter/foundation.dart';
 import 'package:get/get.dart';
+import 'package:just_audio/just_audio.dart';
+import 'package:yeongdu_music_flutter/util/audio_player.dart';
 
 class SeekController extends GetxController {
-  RxDouble duration = 0.0.obs;
-  RxDouble position = 0.0.obs;
-  Duration? bufferedPosition;
-  ValueChanged<Duration?>? onChanged;
-  ValueChanged<Duration?>? onChangeEnd;
+  final SoundPlayer _soundPlayer = SoundPlayer();
 
-  get pos => position;
-  get dur => duration;
+  Rx<Duration?> duration = const Duration(milliseconds: 0).obs;
+  Rx<Duration> position = const Duration(milliseconds: 0).obs;
+  Rx<Duration> bufferedPosition = const Duration(milliseconds: 0).obs;
 
-  setPosition(double pos) {
-    position.value= pos;
-    position.refresh();
+  Rx<double?> dragValue = Rx<double?>(null);
+
+  void init() {
+    duration.value = _soundPlayer.audioPlayer.duration ?? const Duration(milliseconds: 0);
+    position.bindStream(_soundPlayer.audioPlayer.positionStream);
+    bufferedPosition.bindStream(_soundPlayer.audioPlayer.bufferedPositionStream);
   }
 
-  setDuration(double dur) {
-    position.value = dur;
+  void setDragValue(double pos) {
+    dragValue.value = pos;
+    update();
+  }
+
+  void removeDragValue() {
+    dragValue = Rx<double?>(null);
   }
 }
--- lib/screen/player.dart
+++ lib/screen/player.dart
@@ -43,6 +43,7 @@ class Music extends StatelessWidget {
   final SeekController seekController = Get.find();
 
   final _player = SoundPlayer();
+  double? _dragValue;
   final SongModel model;
 
   Music({Key? key, required this.model}) : super(key: key);
@@ -51,17 +52,18 @@ class Music extends StatelessWidget {
   Widget build(context) {
     return Scaffold(
         body: SafeArea(
-      child: body(),
-    ));
+          child: body(),
+        ));
   }
 
   Stream<PositionData> get _positionDataStream =>
       rxdart.Rx.combineLatest3<Duration, Duration, Duration?, PositionData>(
-        _player.audioPlayer.positionStream,
-        _player.audioPlayer.bufferedPositionStream,
-        _player.audioPlayer.durationStream,
-          (position, bufferedPosition, duration) => PositionData(position, bufferedPosition, duration ?? Duration.zero
-          )
+          _player.audioPlayer.positionStream,
+          _player.audioPlayer.bufferedPositionStream,
+          _player.audioPlayer.durationStream,
+              (position, bufferedPosition, duration) =>
+              PositionData(position, bufferedPosition, duration ?? Duration.zero
+              )
       );
 
   Widget body() {
@@ -73,27 +75,24 @@ class Music extends StatelessWidget {
             children: [
               ArtworkImage(id: model.id),
               MusicInfo(title: model.title, artist: model.artist ?? 'Unknown Artist'),
-              StreamBuilder<PositionData>(
-                stream: _positionDataStream,
-                builder: (context, snapshot) {
-                  final positionData = snapshot.data;
+              Obx(() {
+                seekController.init();
 
-                  return Slider(
-                      min: 0.0,
-                      max: positionData!.duration.inMilliseconds.toDouble(),
-                      onChanged: (double value) {
-                        log(value.toString());
-                        seekController.setPosition(value);
-                        log(seekController.position.toString());
-                      },
-                      onChangeEnd: (double value) {
-                        _player.audioPlayer.seek(Duration(milliseconds: value.toInt()));
-                        seekController.setPosition(value);
-                      },
-                      value: positionData.position.inMilliseconds.toDouble()
-                  );
-                }
-              ),
+                return Slider(
+                  min: 0.0,
+                  max: seekController.duration.value == null ? 1.0 : seekController.duration.value!.inMilliseconds.toDouble(),
+                  onChanged: (double value) {
+                    seekController.setDragValue(value);
+                    log(seekController.dragValue.toString());
+                  },
+                  onChangeEnd: (double value) {
+                    _player.audioPlayer.seek(Duration(milliseconds: value.toInt()));
+                    seekController.removeDragValue();
+                    log(seekController.dragValue.toString());
+                  },
+                  value: seekController.dragValue.value ?? seekController.position.value.inMilliseconds.toDouble(),
+                );
+              }),
               FutureBuilder(future: _player.audioPlayer.setUrl(model.uri as String), builder: (context, snapshot) {
                 if (snapshot.hasData == false) {
                   return const CircularProgressIndicator();
